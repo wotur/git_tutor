2b0bf97ead0e35e8e81b50488e03861904f2741f
unknown <s.frayberg@yandex.ru>
2024-07-13 21:41:15 +0700

    add new file

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/random_number')
+def random_number():
+    return str(random.randint(1, 10))
+
+if __name__ == '__main__':
+    app.run(debug=True)
